df2c0f8edba5a4bd4d4fbbbe2c74a439c24c0e3d
Tinoxin09 <tnnnhopi@gmail.com>
2022-09-15 10:08:41 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - returns the product of 2 ints
+ * @a: an int given by the int main function
+ * @b: an int given by the int main function
+ * Description: Uses headers to link and a nested loops to achieve goal
+ * Return: see 1st line
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -3,3 +3,7 @@
 a function that checks for uppercase character.
 
 1. a function that checks for a digit (0 through 9).
+
+2. a function that multiplies two integers.
+
+
