c63fe10584cc3451da7ea27f4dfe3875f9e9e894
Tinoxin09 <tnnhopi@gmail.com>
2022-10-03 22:33:25 +0200

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints its name + \n
+ * @argc: number of agruments typed
+ * @argv: array pointing to arguements
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
